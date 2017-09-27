1a646452b04bcca2fe0746962a4e542d37f2996b
Manuel Ortega <manuel@lasindias.coop>
2017-09-27 15:40:21 +0200

    Iterating over all the cw variants existing in the db, not just the first one

--- db/migrate/20170925162735_inline_forms_create_variants.rb
+++ db/migrate/20170925162735_inline_forms_create_variants.rb
@@ -13,30 +13,34 @@ class InlineFormsCreateVariants < ActiveRecord::Migration
     end
 
     # Adding 'aw' variants and setting the universal ones 'pap'
-    File.open('aw_variants.csv', 'r').each_line do |line|
+    aw_variants_file = File.join Rails.root, 'stuff/aw_variants.csv'
+    File.open(aw_variants_file, 'r').each_line do |line|
       line = line.strip
       cw_lemma, aw_lemma = line.split ','
       # Go to the next row if Aruba variant is empty
       next if aw_lemma == 'x'
 
       # Search the Curacao variant in the databaase
-      variant = Variant.find_by_lemma(cw_lemma)
+      variants = Variant.where(lemma: cw_lemma)
 
-      next if variant.nil?
+      next if variants.empty?
 
-      if variant.lemma == aw_lemma
-        # If the variants are equal, set the existing one as "pap"
-        # "pap" means universal across papiamentu orthographies
-        variant.orthographic_type = 'pap'
-        variant.save
-      elsif Variant.find_by_lemma(aw_lemma).nil?
-        # If the variants are different and
-        # it is not already in the database
-        # create the new one with Aruba orthography
-        variant.word.variants.create(
-          lemma: aw_lemma,
-          orthographic_type: 'aw'
-        )
+      variants.each do |variant|
+        if variant.lemma == aw_lemma
+          # If the variants are equal, set the existing one as "pap"
+          # "pap" means universal across papiamentu orthographies
+          variant.orthographic_type = 'pap'
+          variant.save
+        elsif Variant.where(lemma: aw_lemma, word_id: variant.word.id).empty?
+          # If the variants are different and
+          # it is not already in the database attached
+          # with th cw variant in this loop iteration
+          # then, create the new one with Aruba orthography
+          variant.word.variants.create(
+            lemma: aw_lemma,
+            orthographic_type: 'aw'
+          )
+        end
       end
     end
   end
