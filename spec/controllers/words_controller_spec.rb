09f407ec675ec51918213edb7845271137184ffb
Manuel Ortega <manuel@lasindias.coop>
2018-01-18 17:51:23 -0400

    Fixed WordController specs GET INDEX in order to work ok. Also reverted not needed hot fix

--- spec/controllers/words_controller_spec.rb
+++ spec/controllers/words_controller_spec.rb
@@ -2,8 +2,10 @@ require 'rails_helper'
 
 RSpec.describe WordsController do
   describe "GET index" do
-    it "assigns @objects even when update=words_list is not sended" do
+    it "Successfully" do
+      sign_in create(:user, :superadmin)
       get :index
+      expect(response.status).to eq(200)
       expect(assigns(:objects)).to eq([])
     end
   end
