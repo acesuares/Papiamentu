418a10ec7f50d54d961d237c0b524263c6f9fa9c
Manuel Ortega <manuel@lasindias.coop>
2018-02-01 12:37:16 -0400

    Added frontend for Glossary functionality

--- app/controllers/frontends_controller.rb
+++ app/controllers/frontends_controller.rb
@@ -27,6 +27,40 @@ class FrontendsController < ApplicationController
     end
   end
 
+  def glosario
+    if params[:glossary].blank?
+      redirect_to '/'
+    else
+      @search_glossary = params[:glossary].squish.gsub(CHARACTER_REGEX,'')
+      @glossary = Glossary.find_by_name(@search_glossary)
+      if @glossary.nil?
+        redirect_to '/'
+      end
+    end
+    respond_to do |format|
+      format.html {
+      }
+      format.pdf {
+        html = render_to_string(layout: 'pdf')
+        filename = "#{Time.now.to_s(:db)[0..9]}_#{@glossary.name.strip}.pdf"
+        if Rails.env.production?
+         # generate the pdf
+         html = html.gsub(/\/assets\//, 'https://sazon.id-arte.net/assets/')
+         html = html.gsub(/\/uploads\//, 'https://sazon.id-arte.net/uploads/')
+         pdf = PDFKit.new(html).to_pdf
+         send_data(pdf, :filename => filename, :type => 'application/pdf')
+        else
+         # generate the pdf
+         html = html.gsub(/\/assets\//, 'http://127.0.0.1:3000/assets/')
+         html = html.gsub(/\/uploads\//, 'http://127.0.0.1:3000/uploads/')
+         File.open("#{Rails.root}/pdfs/#{filename}.html", "w+b") {|f| f.write(html)}
+         pdf = PDFKit.new(html).to_pdf
+         send_data(pdf, :filename => filename, :type => 'application/pdf')
+        end
+       }
+    end
+  end
+
   def tra_palabra
     if params[:word].blank? || params[:commit] == "nò, mi ke bai bèk na e pagina inisio"
       redirect_to '/'
--- app/helpers/frontends_helper.rb
+++ app/helpers/frontends_helper.rb
@@ -10,4 +10,16 @@ module FrontendsHelper
       link_to word.name, "/palabra/#{word.name}" unless word.nil?
     end
   end
+
+  def link_to_glossary(glossary, full_url=false)
+    if full_url
+      if Rails.env.development?
+        link_to glossary.name, "http://localhost:3000/glosario/#{glossary.name}" unless glossary.nil?
+      else
+        link_to glossary.name, "https://www.papiamentu.info/glosario/#{glossary.name}" unless glossary.nil?
+      end
+    else
+      link_to glossary.name, "/glosario/#{glossary.name}" unless glossary.nil?
+    end
+  end
 end
