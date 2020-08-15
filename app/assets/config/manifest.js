20f6d1e1e88e26bbce8966b011d4d491a79785ea
Ace Suares <ace@suares.com>
2020-08-15 14:31:29 -0400

    Memory Game, next try

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,6 @@
 //= link_directory ../javascripts .js
 //= link_directory ../javascripts/memory .js
 //= link_directory ../stylesheets .css
-//= link_directory ../stylesheets/memory .css
+//= link_directory ../stylesheets/memory .scss
 //= link_directory ../stylesheets .scss
 //= link inline_forms/inline_forms.css
