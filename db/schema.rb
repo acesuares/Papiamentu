f7e1688395be6e2b9cf55cccb0c4cc795751df70
Ace Suares <ace@suares.com>
2020-11-20 15:49:41 -0400

    add even more flora

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_20_000002) do
+ActiveRecord::Schema.define(version: 2020_11_20_000007) do
 
   create_table "ckeditor_assets", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "data_file_name", null: false
