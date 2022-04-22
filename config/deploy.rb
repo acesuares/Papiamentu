d117eb3f4499984ee8cbc9aa464ce7251fd2cc3d
Ace Suares <ace@suares.an>
2022-04-22 16:08:09 -0400

    csv voor sources

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,6 +56,7 @@ namespace :rvm1 do # https://github.com/rvm/rvm1-capistrano3/issues/45
   desc "Install Bundler"
   task :install_bundler do
     on release_roles :all do
+      execute "cd #{release_path} && #{fetch(:rvm1_auto_script_path)}/rvm-auto.sh . gem update --system"
       execute "cd #{release_path} && #{fetch(:rvm1_auto_script_path)}/rvm-auto.sh . gem install bundler"
       execute "cd #{release_path} && #{fetch(:rvm1_auto_script_path)}/rvm-auto.sh . gem uninstall -I --all mini_racer libv8-node"
       execute "cd #{release_path} && #{fetch(:rvm1_auto_script_path)}/rvm-auto.sh . gem install libv8-node --platform x86_64-linux"
